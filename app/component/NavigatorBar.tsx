ae052f677f1545e818418e8f5df94dd81615b481
rira <73813352+rira0101@users.noreply.github.com>
2025-01-22 14:03:43 +0900

    Header-change

--- app/component/NavigatorBar.tsx
+++ app/component/NavigatorBar.tsx
@@ -1,56 +1,46 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { categoryList, pageList } from "../data/movePageList";
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 // カテゴリごとのリスト取得
 const getCategoryList = (category: string) =>
   pageList.filter((pageInfo) => pageInfo.category === category);
 
 const NavigatorBar = () => {
-  const [hoverItem, setHoverItem] = useState<string>(""); // ホバー中のカテゴリ
   const router = useRouter();
 
-  // ホバー中のカテゴリに対応するリストを取得
-  const displayedList = hoverItem ? getCategoryList(hoverItem) : [];
-  // const displayedList = getCategoryList("rent");
-
   return (
-    <div className="relative mb-6">
+    <div className="flex justify-around bg-gray-100 mb-8 w-full">
       {/* ナビゲーションバー */}
-      <nav
-        style={{ backgroundColor: "#D9E5FF" }}
-        className="h-12 bg-blue-500 flex items-center justify-around px-40 relative"
-      >
-        {categoryList.map(({ category, text }) => (
-          <div
-            style={{ color: "#000066" }}
-            key={category}
-            className="h-12 text-lg font-bold relative flex items-center justify-center"
-            onMouseEnter={() => setHoverItem(category)}
-            onMouseLeave={() => setHoverItem("")}
-          >
-            {text}
-            {/* ホバー時に表示されるリスト */}
-            {hoverItem === category && displayedList.length > 0 && (
-              <ul
-                // style={{ marginTop: "136px", marginLeft: "140px" }}
-                className="absolute top-full left-0 bg-gray-200 shadow-lg p-4 w-max "
-              >
-                {displayedList.map((page) => (
-                  <li
-                    key={page.text}
-                    className="text-black hover:text-blue-500 cursor-pointer whitespace-nowrap"
-                    onClick={() => router.push(page.path)}
-                  >
-                    {page.text}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        ))}
-      </nav>
+      {categoryList.map(({ category, text }) => (
+        <DropdownMenu key={category}>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="w-1/4 bg-gray-200">
+              {text}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              {getCategoryList(category).map((page) => (
+                <DropdownMenuItem
+                  key={page.text}
+                  onClick={() => router.push(page.path)}
+                >
+                  {page.text}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ))}
     </div>
   );
 };
